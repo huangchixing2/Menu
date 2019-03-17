a64ba4a12d42c0af77b5f39bbdef0dadb42f067a
huangchixing2 <15007100341@163.com>
2019-03-18 07:29:46 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/menu/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.administrator.menu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this, "you click add", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "you click remove", Toast.LENGTH_LONG).show();
+                break;
+            default:
+        }
+
+        return true;
+    }
+}
